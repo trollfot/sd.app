6c1f19586bd451bfce7929660456b5c8cd4d0908
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-09-08 21:17:51 +0000

    Corrected templating flaws.
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.app/trunk@201 82af7df8-bc4b-4ebc-8022-2999806f7efb

--- sd/app/rendering/paragraphs.py
+++ sd/app/rendering/paragraphs.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+from Acquisition import aq_base
 
 class ImageContentRenderer(object):
     """Basic renderer for an object with an image.
-    """    
+    """
+    def getSize(self):
+        method = getattr(aq_base(self.context), "getImage_scale", None)
+        return method is not None and method() or 'thumb'
+    
     @property
     def image(self):
-        return self.context.tag(scale='thumb')
+        return self.context.tag(scale=self.getSize())
