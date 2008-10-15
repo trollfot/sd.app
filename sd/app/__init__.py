1b4d08b26ccec8ccdc4f1dced68553e124f0e4d4
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-10-15 20:15:19 +0000

    Corrected namespace
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.app/trunk@245 82af7df8-bc4b-4ebc-8022-2999806f7efb

--- sd/app/__init__.py
+++ sd/app/__init__.py
@@ -2,11 +2,5 @@
 
 PACKAGE = "sd.app"
 
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
-
 import catalog
 import security
