338cc687b45ffe4fba254d3ccf935df75bd5bd59
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-11-12 15:21:17 +0000

    Added templates files
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.app/branches/sd.app-2.5-simplification@357 82af7df8-bc4b-4ebc-8022-2999806f7efb

--- sd/app/rendering/config/viewlets.py
+++ sd/app/rendering/config/viewlets.py
@@ -1,33 +1,39 @@
 # -*- coding: utf-8 -*-
 
 from five import grok
-from zope.interface import Interface
 from zope.component import queryUtility
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.layout.viewlets.common import ViewletBase
-from sd.config.interfaces import IConfigurationSheetType
 from sd.app.rendering.browser.viewlets import AboveRendererBody
+from zope.cachedescriptors.property import CachedProperty
+from sd.config.interfaces import IConfigurationSheetType
+from sd.contents.interfaces import IStructuredItem
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory("sd")
 
 
 class LayoutConfiguration(grok.Viewlet):
-    grok.context(Interface)
+    grok.order(60)
+    grok.context(IStructuredItem)
     grok.viewletmanager(AboveRendererBody)
-    grok.require('zope2.View')
+    grok.require('cmf.ModifyPortalContent')
 
-    render = ViewPageTemplateFile("config.pt")
-
-    @property
+    @CachedProperty
     def layout(self):
         return self.view.__view_name__
 
-    @property
+    @CachedProperty
     def configurable(self):
-        util = queryUtility(IConfigurationSheetType, self.layout, None)
-        return bool(util)
+        return bool(self.view.configurable)
 
-    @property
+    @CachedProperty
     def url(self):
         if self.view.configuration is not None or self.configurable:
             return "%s/++configuration++%s" % (self.context.absolute_url(),
                                                self.layout)
         return None
+
+    def render(self):
+        if not self.url:
+            return u''
+        return u'''<p class="configuration-sheet discreet">
+                   <a href="%s">%s</a>
+                  </p>''' % (self.url, _("Display configuration"))
