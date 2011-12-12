dc7ff20e60e76b22600ba2663b346971aa670879
Sylvain Viollon <thefunny@gmail.com>
2011-12-12 18:06:46 +0100

    Update for latest version of Plone 3.

--- sd/app/catalog.py
+++ sd/app/catalog.py
@@ -1,5 +1,11 @@
 # We register the indexable attribute.
-
+from zope.interface import Interface
+from plone.indexer.decorator import indexer
 from sd.common.catalog.indexable_attributes import hasImageAndCaption
-from Products.CMFPlone.CatalogTool import registerIndexableAttribute
-registerIndexableAttribute('hasImageAndCaption', hasImageAndCaption)
+from Products.CMFCore.utils import getToolByName
+
+@indexer(Interface)
+def hasImageAndCaptionFactory(content):
+    return hasImageAndCaption(
+        content,
+        getToolByName('portal_tool', content).getPortalObject())
